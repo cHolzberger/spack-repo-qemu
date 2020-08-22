c5f7d5e4a9fff7bb4ce2aa36c35320f5d2ebf947
C. Holzberger <ch@mosaiksoftware.de>
2020-08-22 13:21:13 +0200

    qemu 5.1

--- packages/qemu/package.py
+++ packages/qemu/package.py
@@ -36,7 +36,6 @@ class Qemu(AutotoolsPackage):
     depends_on("curl")
     depends_on("glib")
     depends_on("pixman")
-    depends_on("numactl")
     depends_on("libaio")
     depends_on("liburing")
     depends_on("libiscsi")
@@ -54,11 +53,13 @@ class Qemu(AutotoolsPackage):
     version('4.2.0', sha256='d3481d4108ce211a053ef15be69af1bdd9dde1510fda80d92be0f6c3e98768f0')
     version('5.0.0-rc4', sha256='5cbec3102322cfe9810a9628aa85f373d71167c2e4be628db0f03fc5dd43799f')        
     version('5.0.0', sha256='2f13a92a0fa5c8b69ff0796b59b86b080bbb92ebad5d301a7724dd06b5e78cb6')
+    version('5.1.0', sha256='c9174eb5933d9eb5e61f541cd6d1184cd3118dfe4c5c4955bc1bdc4d390fa4e5')
 
     patch('https://github.com/saveriomiroddi/qemu-pinning/commit/4e4fe6402e9e4943cc247a4ccfea21fa5f608b30.patch', sha256='c6b69ec2820605e24ff490dceefebf78768dc816a1bdaf43ab32b25976e4582e', when='@4.2.0')
     patch('qemu-60fps.patch', sha256='5ba40805df20aaac369d8e217f3fa0e8d7e29a12ca98908034e7062aafcfcc98')
     patch('qemu-pinning-v5.0.0-rc4.patch', sha256='5227f82cf3be903e0b84717c7662dafaf0e3e08808cccda2d5c3abaac6525f16', when='@5.0.0-rc4')
     patch('qemu-pinning-v5.0.0-rc4.patch', sha256='5227f82cf3be903e0b84717c7662dafaf0e3e08808cccda2d5c3abaac6525f16', when='@5.0.0')
+    patch('qemu-pinning-v5.1.0.patch', when='@5.1.0')
 
     @run_after('install')
     def add_tools(self):
